07e889ef1f687423c67024eb8b7e12b7e05ecfdc
Army-U <wuchengkai0@gmail.com>
2019-12-05 11:02:55 +0800

    refactor(*): use optional chaining instead of an expression

--- babel.config.js
+++ babel.config.js
@@ -12,6 +12,11 @@ module.exports = {
     '@babel/preset-typescript'
   ],
   plugins: [
+    /**
+     * @see https://github.com/babel/babel/issues/10690#issuecomment-552979676
+     */
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-optional-chaining',
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
     ['@babel/plugin-syntax-dynamic-import'],
