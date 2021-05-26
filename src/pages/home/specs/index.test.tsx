3c3c821e607276df127e3df3cf6279fe97c53218
Jeremy Wu <jeremy.wu01@sap.com>
2021-05-26 16:26:06 +0800

    tweak

--- src/pages/home/specs/index.test.tsx
+++ src/pages/home/specs/index.test.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import configureStore from 'redux-mock-store'
+import { configureStore } from '@reduxjs/toolkit'
 
 import Home from 'src/pages/home'
 import { mount } from '@cypress/react'
+import reducer from 'src/pages/home/store/slice'
 
-const mockStore = configureStore()
+const store = configureStore({
+  reducer: {
+    home: reducer,
+  },
+})
 
 describe('Home', () => {
   it('render no error', () => {
     mount(
-      <Provider store={mockStore()}>
+      <Provider store={store}>
         <Home />
       </Provider>,
     )
